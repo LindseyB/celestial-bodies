0293ba51d2e8b3513b6a57c3cc214b70b792f804
Lindsey Bieda <hazardous.narayan@gmail.com>
2016-03-12 14:42:39 -0500

    Adding starting project junk

---
+++ Assets/initialVelocity.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class initialVelocity : MonoBehaviour {
+	public Rigidbody2D rb;
+	public Vector2 force;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		force = new Vector2(-500, 0);
+		rb.AddForce(force);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//rb.AddForce(force);
+	}
+}
